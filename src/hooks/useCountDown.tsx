bbbba6ba4b6669906c00d3281576982d5281e187
Pernilla Skeppstedt <pernilla.skeppstedt@gmail.com>
2022-12-13 12:53:34 +0100

    remove comments and update function

--- src/hooks/useCountDown.tsx
+++ src/hooks/useCountDown.tsx
@@ -1,10 +1,6 @@
 import { useState, useRef, useEffect } from "react";
 
-export const useCountDown = (
-  initialValue: number,
-  onComplete?: () => void,
-  restartValues: Array<any> = []
-) => {
+export const useCountDown = (initialValue: number, onComplete?: () => void) => {
   let [counter, setCounter] = useState(initialValue);
   let [timer, setTimer] = useState<any>(null);
   const intervalRef = useRef<any>();
@@ -15,7 +11,7 @@ export const useCountDown = (
   useEffect(() => {
     setCounter(initialValue);
     const _timer = setInterval(() => {
-      if (intervalRef.current == 0) {
+      if (intervalRef.current === 0) {
         if (onComplete) {
           onComplete();
         }
@@ -26,8 +22,8 @@ export const useCountDown = (
     }, 1000);
     setTimer(_timer);
     return () => clearInterval(_timer);
-  }, restartValues); // trigger new timer
-  return [counter, interrupt] as const; // return interrupt
+  }, [initialValue, onComplete]);
+  return [counter, interrupt] as const;
 };
 
 //Found this example from https://kodaps.com/creating-a-custom-hook-usecountdown-creating-a-react-quiz-app-part-2-72c82ca83b49 but changed it a bit.
