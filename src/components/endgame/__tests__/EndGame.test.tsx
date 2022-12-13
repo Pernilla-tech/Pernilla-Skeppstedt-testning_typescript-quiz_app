f0524e30b3b0b66c9d1d823a6e4cb86ab163da31
Pernilla Skeppstedt <pernilla.skeppstedt@gmail.com>
2022-12-13 12:31:29 +0100

    update endGame test

--- src/components/endgame/__tests__/EndGame.test.tsx
+++ src/components/endgame/__tests__/EndGame.test.tsx
@@ -1,8 +1,17 @@
 import { render, screen } from "@testing-library/react";
 import EndGame from "../EndGame";
 
-it("Render h1 tag", () => {
-  render(<EndGame />);
-  const titleElement = screen.getByText(/End game/i);
-  expect(titleElement).toBeInTheDocument();
+describe("Render EndGame correctly", () => {
+  it("Render h1 tag", () => {
+    render(<EndGame />);
+    const titleElement = screen.getByText(/End game/i);
+    expect(titleElement).toBeInTheDocument();
+  });
+
+  it("Render a Play again button", () => {
+    render(<EndGame />);
+    const buttonElement = screen.getByText(/Play again/i);
+    expect(buttonElement).toBeInTheDocument();
+    expect(buttonElement).toBeEnabled();
+  });
 });
